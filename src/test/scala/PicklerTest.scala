1341098b3f0124fb0b6c9fcb7ff0d0b537f0ec4a
Michael Pollmeier <michael@michaelpollmeier.com>
2016-03-29 10:53:29 +1300

    custom serialiser

--- src/test/scala/PicklerTest.scala
+++ src/test/scala/PicklerTest.scala
@@ -1,26 +1,46 @@
-import io.github.netvl.picopickle.backends.collections.CollectionsPickler._
 import org.scalatest.Matchers
 import org.scalatest.WordSpec
+import io.github.netvl.picopickle.{BackendComponent, TypesComponent}
+import io.github.netvl.picopickle.backends.collections.CollectionsPickler
 
 class PicklerTest extends WordSpec with Matchers {
   import CaseClasses._
 
   "simple case class" in {
-    write(Simple(10, "hi")) shouldBe Map("x" -> 10, "y" -> "hi")
-    read[Simple](Map("x" -> 10, "y" -> "hi")) shouldBe Simple(10, "hi")
+    import CollectionsPickler._
+    write(Simple(10, "hi")) shouldBe Map("x" → 10, "y" → "hi")
+    read[Simple](Map("x" → 10, "y" → "hi")) shouldBe Simple(10, "hi")
   }
 
   "unwraps Some and None" in {
-    write(WithOption(Some("hi"))) shouldBe Map("so" -> "hi")
+    import CollectionsPickler._
+    write(WithOption(Some("hi"))) shouldBe Map("so" → "hi")
     write(WithOption(None)) shouldBe Map.empty
 
-    read[WithOption](Map("so" -> "hi")) shouldBe WithOption(Some("hi"))
+    read[WithOption](Map("so" → "hi")) shouldBe WithOption(Some("hi"))
     read[WithOption](Map.empty) shouldBe WithOption(None)
   }
 
+  "custom pickler" in {
+    object DefinedByIntPickler extends CollectionsPickler {
+      implicit val definedByIntWriter: Writer[Simple] = Writer {
+        case Simple(x, _) ⇒ backend.makeNumber(x)
+      }
+
+      implicit val definedByIntReader: Reader[Simple] = Reader {
+        case backend.Extract.Number(x) ⇒ Simple(x.intValue(), x.intValue().toString)
+      }
+    }
+    import DefinedByIntPickler._
+
+    write(Simple(10, "hi")) shouldBe 10
+    read[Simple](10) shouldBe Simple(10, "10")
+  }
+
   "unwraps AnyVal" in {
-    write(WithValueClass(10, MyValueClass("hi"))) shouldBe Map("i" -> 10, "vc" -> "hi")
-    read[WithValueClass](Map("i" -> 10, "vc" -> "hi")) shouldBe WithValueClass(10, MyValueClass("hi"))
+    import CollectionsPickler._
+    write(WithValueClass(10, MyValueClass("hi"))) shouldBe Map("i" → 10, "vc" → "hi")
+    read[WithValueClass](Map("i" → 10, "vc" → "hi")) shouldBe WithValueClass(10, MyValueClass("hi"))
   }
 
 }
